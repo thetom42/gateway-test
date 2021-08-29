103932cf0bf7eab12f060e8aeb8df5647e52b0de
Thomas Hessel <thetom42@gmail.com>
2021-08-29 20:07:28 +0000

    Fix shell script

--- gt.sh
+++ gt.sh
@@ -1,1 +1,4 @@
-docker run -it -p 8880:8080 -p 8890:8090 -v /home/thetom42/repos/thetom42/go/gateway-test:/etc/krakend --name gt gateway-test:0.1
+docker stop gt >> /dev/null 2>&1
+docker rm gt >> /dev/null 2>&1
+docker build -t gateway-test:0.1 -f Dockerfile .
+docker run -it -p 8880:8080 -p 8890:8090 -v $PWD:/etc/krakend --name gt gateway-test:0.1
